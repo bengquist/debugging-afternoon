695b70f7293aace751cca01cf75ab25f5396b393
Blake Engquist <engquist_blake@yahoo.com>
2018-06-26 17:11:05 -0500

    completed

--- stage-1/src/components/Home/Home.js
+++ stage-1/src/components/Home/Home.js
@@ -1,11 +1,11 @@
-import './Home.css';
+import "./Home.css";
+import { Component } from "react";
+import React from "react";
 
 class Home extends Component {
-    render() {
-        return (
-            <div className="home-landing"></div>
-        )
-    }
+  render() {
+    return <div className="home-landing" />;
+  }
 }
 
 export default Home;
